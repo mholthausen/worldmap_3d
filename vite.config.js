b8c2a52cee0110afd7a81c60478dcde8cfc15afa
Michael Holthausen <mholthausen@terrestris.de>
2025-07-22 13:50:40 +0200

    fix: fixes base path

--- vite.config.js
+++ vite.config.js
@@ -7,23 +7,24 @@ const cesiumSource = "node_modules/cesium/Build/Cesium";
 const cesiumBaseUrl = "cesiumStatic";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  define: {
-    // Define relative base path in cesium for loading assets
-    // https://vitejs.dev/config/shared-options.html#define
-    CESIUM_BASE_URL: JSON.stringify(`/${cesiumBaseUrl}`),
-  },
-  plugins: [
-    // Copy Cesium Assets, Widgets, and Workers to a static directory.
-    // If you need to add your own static files to your project, use the `public` directory
-    // and other options listed here: https://vitejs.dev/guide/assets.html#the-public-directory
-    viteStaticCopy({
-      targets: [
-        { src: `${cesiumSource}/ThirdParty`, dest: cesiumBaseUrl },
-        { src: `${cesiumSource}/Workers`, dest: cesiumBaseUrl },
-        { src: `${cesiumSource}/Assets`, dest: cesiumBaseUrl },
-        { src: `${cesiumSource}/Widgets`, dest: cesiumBaseUrl },
-      ],
-    }),
-  ],
-});
+export default defineConfig(({ command }) => {
+  const isProduction = command === 'build';
+  console.log(`Vite is running in ${isProduction ? 'production' : 'development'} mode.`);
+
+  return {
+    base: isProduction ? '/worldmap_3d/' : '/',
+    define: {
+      CESIUM_BASE_URL: JSON.stringify(`/${cesiumBaseUrl}`),
+    },
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          { src: `${cesiumSource}/ThirdParty`, dest: cesiumBaseUrl },
+          { src: `${cesiumSource}/Workers`, dest: cesiumBaseUrl },
+          { src: `${cesiumSource}/Assets`, dest: cesiumBaseUrl },
+          { src: `${cesiumSource}/Widgets`, dest: cesiumBaseUrl },
+        ],
+      }),
+    ],
+  };
+})
